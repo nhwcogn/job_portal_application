258fe5958146eaf0dc3cacb2103b09035a14f1a8
Như Ngọc <ngoc89704@gmail.com>
2025-06-08 13:17:02 +0700

    up code

--- job-portal/src/FindJobs/Jobs.tsx
+++ job-portal/src/FindJobs/Jobs.tsx
@@ -1,4 +1,18 @@
-const Jobs=() => {
+import { jobList } from "../Data/JobsData";
+import JobCard from "./JobCard";
+import Sort from "./Sort";
 
+const Jobs=() => {
+    return <div className="p-5">
+        <div className="flex justify-between">
+            <div className="text-2xl font-semibold">Recommended Jobs</div>
+            <Sort/>
+        </div>
+        <div className="mt-10 flex flex-wrap gap-5">
+            {
+            jobList.map((job, item)=> <JobCard key={item} {...job}/>)
+        }
+        </div>
+    </div>
 }
 export default Jobs;
--- job-portal/src/Pages/FindJobs.tsx
+++ job-portal/src/Pages/FindJobs.tsx
@@ -1,11 +1,14 @@
 import { Divider } from "@mantine/core";
 import SearchBar from "../FindJobs/SearchBar";
+import Jobs from "../FindJobs/Jobs";
 
 const FindJobs = () => {
     return (
         <div className="min-h-[100vh] bg-mine-shaft-950">
             <Divider size="xs" mx="md" />
             <SearchBar/>
+            <Divider size="xs" mx="md" />
+            <Jobs/>
         </div>
         
     )
